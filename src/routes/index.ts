4b964162908f58051857ed0f704dc5dad5083676
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2022-11-26 02:03:37 -0300

    Session

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,7 @@ import { MyPlainRoutes } from "./plains/myplain.routes";
 import { paymentsRoutes } from "./plains/payments.routes";
 import { plainsRoutes } from "./plains/plains.routes";
 import { PropertyRoutes } from "./property/property.routes";
+import { SessionClientRouter } from "./sessions/sessions.routes";
 
 const router = Router();
 
@@ -14,6 +15,7 @@ connectToDatabase()
   .then(() => {
     router.use("/company", CompanyRouter)
     router.use("/client", AccountClientRouter);
+    router.use("/session", SessionClientRouter);
     router.use("/property", PropertyRoutes);
     router.use("/plains", plainsRoutes);
     router.use("/myplain", MyPlainRoutes);
