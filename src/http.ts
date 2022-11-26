a03344f093026f3a62fc0704152c6b682c12780c
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2022-11-25 23:10:01 -0300

    Teste de zerar algumas coisas

--- src/http.ts
+++ src/http.ts
@@ -22,7 +22,7 @@ app.use(router);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://www.forpride.com.br", "https://adm.forpride.com.br", "http://localhost:3000"],
+    origin: ["https://www.suachave.com.br", "https://adm.suachave.com.br", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials: true
   }
