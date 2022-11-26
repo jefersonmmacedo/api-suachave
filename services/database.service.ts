d95e6ccdde4eab1f064b39c9d2e8eadb3ecf258c
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2022-11-25 22:33:30 -0300

    Alterações

--- services/database.service.ts
+++ services/database.service.ts
@@ -31,7 +31,9 @@ export async function connectToDatabase() {
   dotenv.config();
 
   const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
+
   await client.connect();
+
   const db: mongoDB.Db = client.db(process.env.DB_NAME);
 
       // company
