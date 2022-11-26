82b77b3abe2dd3c364992a12511352d774f9d8c9
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2022-11-25 23:59:34 -0300

    Teste de zerar algumas coisas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { connectToDatabase } from "../../services/database.service";
+import { CompanyRouter } from "./accounts/company.routes";
 
 const router = Router();
 
 
 connectToDatabase()
   .then(() => {
+    router.use("/company", CompanyRouter)
   }).catch((error: Error) => {
     console.error("Database connection failed", error);
     process.exit();
