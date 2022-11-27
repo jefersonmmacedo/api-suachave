9f76c5a0979df63361b71b08c2c3965c7bf4b0fd
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2022-11-26 21:11:12 -0300

    Chats and Messages

--- src/routes/message/chats.routes.ts
+++ src/routes/message/chats.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { createChatsController } from "../../Modules/Message/useCases/createChats";
+import { deleteChatsController } from "../../Modules/Message/useCases/deleteChats";
+import { listChatsController } from "../../Modules/Message/useCases/listChats";
 
 
 
@@ -7,6 +9,12 @@ const chatsRoutes = Router();
 
 chatsRoutes.post("/", (req, res) => {
   return createChatsController.handle(req, res);
+});
+chatsRoutes.get("/", (req, res) => {
+  return listChatsController.handle(req, res);
+});
+chatsRoutes.delete("/", (req, res) => {
+  return deleteChatsController.handle(req, res);
 });
 
 
--- src/routes/message/messages.routes.ts
+++ src/routes/message/messages.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { createMessagesController } from "../../Modules/Message/useCases/createMessages";
+import { deleteMessagesController } from "../../Modules/Message/useCases/deleteMessages";
+import { listMessagesController } from "../../Modules/Message/useCases/listMessages";
 
 
 const MessagesRoutes = Router();
 
 MessagesRoutes.post("/", (req, res) => {
   return createMessagesController.handle(req, res);
+});
+MessagesRoutes.get("/", (req, res) => {
+  return listMessagesController.handle(req, res);
+});
+MessagesRoutes.delete("/", (req, res) => {
+  return deleteMessagesController.handle(req, res);
 });
 
 export { MessagesRoutes };
