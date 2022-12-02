f6f59ef0bb862401ca43ccae1791006fe040415c
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2022-12-01 23:49:04 -0300

    Novos endereços

--- src/Modules/Property/useCases/listAccountProperty/ListPropertyController.ts
+++ src/Modules/Property/useCases/listAccountProperty/ListPropertyController.ts
@@ -10,26 +10,10 @@ class ListPropertyController {
 
   async handle(req: Request, res: Response) {
     const {availability, status} = req.params;
-    const uf = req.query.uf;
-    const city = req.query.city;
-    const bedroom = req.query.bedroom;
-    const restroom = req.query.restroom;
-    const garage = req.query.garage;
-    const suite = req.query.garage;
-
-    console.log({bedroom: bedroom})
-    console.log({restroom: restroom})
-    console.log({garage: garage})
 
    await collections.property.find({
     "$and": [{ availability: availability },
-    { status: status},
-    { uf: uf},
-    { city: city},
-    { bedroom: { $gte: bedroom } },
-    { suite: { $gte: suite } },
-    { restroom: { $gte: restroom } },
-    { garage: { $gte: garage } },
+    { status: status}
   ],
 }).sort( { created_at: -1 } ).toArray(function(err, result){
       if(err) {
