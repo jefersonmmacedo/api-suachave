d322ced4f53ca64f09603325e934ec1f260a5ac0
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2022-12-04 23:29:45 -0300

    Novas tabelas de negociações

--- src/routes/negotiations/negotiations.routes.ts
+++ src/routes/negotiations/negotiations.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { createNegotiationsController } from "../../Modules/Negotiations/useCases/createNegotiations";
 import { listNegotiationsController } from "../../Modules/Negotiations/useCases/listNegotiations";
 import { listNegotiationsUnicController } from "../../Modules/Negotiations/useCases/listNegotiationsUnic";
-import { updateNegotiationsController } from "../../Modules/Negotiations/useCases/updateBroker";
+
 
 
 const negotiationsRoutes = Router();
@@ -15,9 +15,6 @@ negotiationsRoutes.get("/", (req, res) => {
 });
 negotiationsRoutes.get("/company/:idCompany", (req, res) => {
   return listNegotiationsUnicController.handle(req, res);
-});
-negotiationsRoutes.get("/company/:idCompany", (req, res) => {
-  return updateNegotiationsController.handle(req, res);
 });
 
 
