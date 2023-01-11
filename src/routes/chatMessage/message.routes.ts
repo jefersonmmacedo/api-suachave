a189567d4f26ef9ba241c0e9197452e2b273a434
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2023-01-10 21:42:39 -0300

    Listando todas as mensagens do usuário

--- src/routes/chatMessage/message.routes.ts
+++ src/routes/chatMessage/message.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createMessageController } from "../../Modules/ChatMessage/useCases/createMessage";
 import { listMessageController } from "../../Modules/ChatMessage/useCases/listMessage";
+import { listMessageClientController } from "../../Modules/ChatMessage/useCases/listMessageClient";
 import { listMessageUserController } from "../../Modules/ChatMessage/useCases/listMessageUser";
 
 
@@ -11,6 +12,9 @@ MessageRouter.post("/", (req, res) => {
 });
 MessageRouter.get("/:idRoom", (req, res) => {
   return listMessageController.handle(req, res);
+});
+MessageRouter.get("/messagesall/:idAccount", (req, res) => {
+  return listMessageClientController.handle(req, res);
 });
 MessageRouter.get("/company/:idCompany", (req, res) => {
   return listMessageUserController.handle(req, res);
